07b7066df2f489b5abd909784d5f153409ab1f45
Marco Condori <condorimarco850@gmail.com>
2024-09-06 15:53:32 -0400

    commit subiendo por segunda vez cd

---
+++ archivo1.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+public class archivo1{
+    public static void main(String[] args){
+        Scanner cin=new Scanner(System.in);
+
+        System.out.println("ingrese el primer numero:");
+        int a=cin.nextInt();
+        System.out.println("ingrese el segundo numero:");
+        int b=cin.nextInt();
+        if(b!=0){
+        if(a%b==0){
+            System.out.println("es divisible:");
+        }else{
+            System.out.println("no es divisible:");
+        }
+    }else{
+        System.out.println("no existen division entre cero;");
+        }
+    }
+}
